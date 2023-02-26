539069bb84f7cd296ce2ccd3b8b41bf22b42a5f0
GubanovIV <113034537+GubanovIV@users.noreply.github.com>
2023-02-26 20:11:16 +0300

    Add files via upload

---
+++ Dialog/N1.java
@@ -0,0 +1,13 @@
+package Stepik.Dialog;
+
+import javax.swing.*;
+
+public class N1 {
+
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Введите имя: ");
+        JOptionPane.showMessageDialog(null,name);
+        JOptionPane.showMessageDialog(null,"Ivan Gubanov");
+    }
+
+}
